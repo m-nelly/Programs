c88db7cb3dd342bc0b2b47cbd6ce0908d0d6e5db
netmn <netmn99@gmail.com>
2020-11-28 14:32:24 -0600

    Added ping sweep functionality.

--- Network Diagnostics/PingSweepSubnet.py
+++ Network Diagnostics/PingSweepSubnet.py
@@ -4,10 +4,30 @@
 
 # Add functionality for additional scanning layers. (Potentially network mapping)
 
-from pythonping import ping
+import pythonping as p
+import ipaddress as ip
 
 # Perform a basic ping of your loopback address with regular output.
 
-q = ping('127.0.0.1', verbose=True)
+test = p.ping('127.0.0.1', count=10)
+print(test._responses[0].success)
 
 # parameters: size(bytes), timout(seconds), count(# of pings), out(defines a location to send verbose data, def sys.stdout)
+
+def pingSweep(startAddress= None, endAddress = None, subnetMask = None, cidrNetwork = None, numPings = 4):
+
+# The purpose of these arguments is to allow the user freedom in how they approach the network. 
+
+    for x in ip.ip_network(cidrNetwork):
+        response = p.ping(str(x), count=4, timeout = 0.025)
+        for y in range(4):
+
+            if response._responses[y].success == True:
+                print(x)
+                break
+            else:
+                continue
+        
+userNetwork = str(input('Network(CIDR): '))
+
+pingSweep(cidrNetwork=userNetwork)
